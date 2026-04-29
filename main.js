63aa9a9310b61cb621da8b2d937a5e0cac4ac02f
kumakero-otama <144969427+kumakero-otama@users.noreply.github.com>
2026-04-29 21:48:24 +0900

    v1.1.1 ・自己紹介文で、入力した名前で自己紹介できるようにした

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const button = document.querySelector("#button")  // ここでいうbuttonとは、自己PRするボタンのことだよ
+const button = document.querySelector("#button")  // buttonの定義
 button.addEventListener("click", function (){  // buttonがクリックされた時の処理
-    const output = document.querySelector("#output");  // ここでいうoutputとは、吹き出し用テキストのことだよ
-    output.textContent =　"Hello Would!!"  // outputに"Hello Would!!"と表示してね
+    const name = document.querySelector("#name").value;  // nameの定義
+    const message = `こんにちは、私の名前は${name}です。`
+    const output = document.querySelector("#output");  // outputの定義
+    output.textContent =　message; 
 })
