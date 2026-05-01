cb11a9901ec210d25e79ca70cf9cb482c5c968d5
kumakero-otama <144969427+kumakero-otama@users.noreply.github.com>
2026-05-01 16:18:36 +0900

    v1.1.3 ・v1.1.2で自己紹介の要素をランダムに選ぶのを忘れていたため、直した

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 趣味、特技、挨拶の設定
+// 趣味、特技、挨拶の要素
 const interests = ["とあるマンガ", "ボカロ鑑賞", "遅寝遅起き"]
 const specialties = ["元素記号を暗唱すること", "美味しいものを食べること", "ゆっくりすること"]
 const greetings = ["よろしく！", "どうぞよろしくお願いいたします。", "お手柔らかに〜"]
@@ -9,13 +9,22 @@ const button = document.querySelector("#button")  // buttonの定義
 button.addEventListener("click", function (){  
     const name = document.querySelector("#name").value;  // nameの定義
     
-    // 表示するコメント
+    // 自己紹介の要素をランダムに選ぶ
+    const index1 = Math.floor(Math.random() * interests.length);
+    const index2 = Math.floor(Math.random() * specialties.length);
+    const index3 = Math.floor(Math.random() * greetings.length);
+    const interest = interests[index1];
+    const specialty = specialties[index2];
+    const greeting = greetings[index3];
+
+    // コメントの内容定義
     const message = `
     こんにちは、私の名前は${name}です。
-    最近${interests}にハマっていて、
-    特技は${specialties}です。
-    ${greetings}`
+    最近${interest}にハマっていて、
+    特技は${specialty}です。
+    ${greeting}`
     
+    // コメント表示
     const output = document.querySelector("#output");  // outputの定義
     output.textContent =　message; 
 })
