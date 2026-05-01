22db2fa8bf232ab1589283dafdfdf5588ac552fc
kumakero-otama <144969427+kumakero-otama@users.noreply.github.com>
2026-05-01 16:08:07 +0900

    v1.1.2 ・自己紹介文で、特技、趣味、あいさつをランダムに表示できるようにした ・P58 2-3まで

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+// 趣味、特技、挨拶の設定
+const interests = ["とあるマンガ", "ボカロ鑑賞", "遅寝遅起き"]
+const specialties = ["元素記号を暗唱すること", "美味しいものを食べること", "ゆっくりすること"]
+const greetings = ["よろしく！", "どうぞよろしくお願いいたします。", "お手柔らかに〜"]
+
 const button = document.querySelector("#button")  // buttonの定義
-button.addEventListener("click", function (){  // buttonがクリックされた時の処理
+
+// buttonがクリックされた時の処理
+button.addEventListener("click", function (){  
     const name = document.querySelector("#name").value;  // nameの定義
-    const message = `こんにちは、私の名前は${name}です。`
+    
+    // 表示するコメント
+    const message = `
+    こんにちは、私の名前は${name}です。
+    最近${interests}にハマっていて、
+    特技は${specialties}です。
+    ${greetings}`
+    
     const output = document.querySelector("#output");  // outputの定義
     output.textContent =　message; 
 })
